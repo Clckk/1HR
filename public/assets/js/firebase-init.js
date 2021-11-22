acdb979e052605fa89d713a458f988f5b0c54415
Clckk <mateusz.palka2013@live.com>
2021-11-22 17:29:15 +0100

    firebasee

---
+++ public/assets/js/firebase-init.js
@@ -0,0 +1,13 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYpdRu6aie31VHta8EeD1f5pL3mMIFCoo",
+  authDomain: "hrboostin-pro.firebaseapp.com",
+  projectId: "hrboostin-pro",
+  storageBucket: "hrboostin-pro.appspot.com",
+  messagingSenderId: "784042279518",
+  appId: "1:784042279518:web:7d75ace7b2c02e53e02acf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
